dbd68fa0533367344a8aa4ea3ea7bde0bcefb95b
ShawnMax <48395061+ShawnMax@users.noreply.github.com>
2021-07-03 01:41:16 +1200

    Add files via upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./wdyr";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { DevTools, loadServer } from "jira-dev-tool";
+// 务必在jira-dev-tool后面引入
+import "antd/dist/antd.less";
+import { AppProviders } from "context";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+loadServer(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <AppProviders>
+        <DevTools />
+        <App />
+      </AppProviders>
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
 );
 
 // If you want to start measuring performance in your app, pass a function
